f4dae40109f87be2a92b3a0f62a7ce0534e38cbe
Saurav Gautam <55937631+sauravgautam320@users.noreply.github.com>
2022-10-01 20:13:45 +0545

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\sauravs_file\\AppData\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\sauravs_file\\programming_files\\android\\flutter\\tictactoe" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\sauravs_file\\AppData\\flutter"
+  "PROJECT_DIR=D:\\sauravs_file\\programming_files\\android\\flutter\\tictactoe"
+  "FLUTTER_ROOT=D:\\sauravs_file\\AppData\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\sauravs_file\\programming_files\\android\\flutter\\tictactoe\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\sauravs_file\\programming_files\\android\\flutter\\tictactoe"
+  "FLUTTER_TARGET=D:\\sauravs_file\\programming_files\\android\\flutter\\tictactoe\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\sauravs_file\\programming_files\\android\\flutter\\tictactoe\\.dart_tool\\package_config.json"
+)
